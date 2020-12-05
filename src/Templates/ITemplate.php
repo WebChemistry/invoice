3f83feaf431d0652389e133515ba53c9d7312ff5
Peter Kracik <peter@kracik.sk>
2020-12-05 12:12:23 +0100

    Using a Formatter interface rather than class for the template constructor

--- src/Templates/ITemplate.php
+++ src/Templates/ITemplate.php
@@ -7,13 +7,13 @@ namespace WebChemistry\Invoice\Templates;
 use WebChemistry\Invoice\Data\Company;
 use WebChemistry\Invoice\Data\Customer;
 use WebChemistry\Invoice\Data\Order;
-use WebChemistry\Invoice\Formatter;
+use WebChemistry\Invoice\IFormatter;
 use WebChemistry\Invoice\ITranslator;
 use WebChemistry\Invoice\Renderers\IRenderer;
 
 interface ITemplate {
 
-	public function __construct(ITranslator $translator, Formatter $formatter);
+	public function __construct(ITranslator $translator, IFormatter $formatter);
 
 	public function build(IRenderer $renderer, Customer $customer, Order $order, Company $company): string;
 
